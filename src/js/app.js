14dae9fe1f85719b5909a26d7605e7401bd74260
NurKhab <nurkhabib2006@gmail.com>
2023-02-10 03:04:35 +0300

    Alpha-Version
    The first version of a website: it almost doesn't have any interesting functions.

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);	
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+			if (burgerMenu.classList.contains('active')) {
+				document.body.classList.remove('lock');
+				burgerMenu.classList.remove('active');
+				menuBody.classList.remove('active');
+			} 
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
