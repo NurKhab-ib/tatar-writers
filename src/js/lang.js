1544d9d25b0485840018377d9b54035ef4a852c5
NurKhab <nurkhabib2006@gmail.com>
2023-03-26 04:29:56 +0300

    Demo

--- src/js/lang.js
+++
@@ -1,8 +0,0 @@
-export const langArr = {
-    "doctitle" : {
-        "ru" : "Татарские писатели XX века",
-        "tat" : "XX гасырнын татар язучылары",
-        "en" : "Tatar writers of XX century",
-    },
-    
-}
